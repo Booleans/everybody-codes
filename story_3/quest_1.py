d51fa844d96331a7fc70c45ea44577a4de30d252
Andrew Nicholls <andrew.nicholls@databricks.com>
2026-04-26 12:06:30 -0700

    Update quest_1.py
    Solve part 2.

--- story_3/quest_1.py
+++ story_3/quest_1.py
@@ -1,11 +1,268 @@
+lines = '''43940:RrrRRr GggGgG bBbbBB SSsSSS
+51780:rrRRRr GgGggG BbbbBB SsssSS
+42885:rRRrrR ggGgGG bbbbBb SsssSS
+30324:RrRrRr gGgGgG BBbBbB SSSSsS
+88068:RrrRrr GGGgGG bbBbBB SSSSSs
+25058:RRRRRr GGGggG BBBbbB sssSss
+18374:RrRRrr gggggG bBBbBB SsssSS
+33641:RRRRRR ggGGgg BBBBbb sSSssS
+27816:rrrrRR GggGGG bbbbBb SsSSSS
+56684:rRrrRr GgGgGG bbbBbB Ssssss
+28605:RrRrRR GGgggG BbBbbb SSsssS
+49384:RrRrRr GGggGG BbbBBB SSSSss
+10136:RRrrrr GggGGG bbBBBB ssSSsS
+17501:RrRrrR gGGgGG bBbBbB sSSsSs
+45412:RRRRrR GGgggG BbBBbB SssSSS
+74934:RRRRRr GGgGGG BbBbbb sSSsss
+97229:rrrrrR ggggGg bBBbbB sSsSss
+27226:rrrrrr GGggGg BBBbBb sSsssS
+64496:rRrrrR gGgGGg BbBbbB sSsSsS
+37889:RrRRRR ggGggG bBBBbB sssSSs
+30276:RRRrRR gGgggG bbBbbB sSsSss
+59280:RRRrRR gGgGgg BBBbBB sSSsSs
+99820:rrrRrr ggGggg BbbbBb SssSSs
+75623:RRRrRR GgGggG bbbbbB SSSSSS
+16374:RRRRrR GgGggg Bbbbbb SSsSss
+24886:rRrrRR gGgggg bbBBbb sSSSSs
+83369:Rrrrrr GgGggg BbbBBB ssSsSs
+35490:RRrRRr GGGgGG BBBbbB ssSSSs
+76521:rRrrRR gGggGg bBbBbB ssSssS
+74092:RRrrRR gGgGgg BBBbbB ssSSss
+47627:rrrRRR GGgGgg bbbbBB sSsSSs
+87521:RRrrRR gGgGgg bBBBbb sSSsss
+34806:RrrrRR GGgGGG BbBBBb SSSSSs
+33569:RrRrrR GgGggg bbBbbB sSsSSS
+57502:rRRrrR gggggG BBbBBB SSsssS
+75802:rRrrrR ggGgGG BbBBBB SSSssS
+54572:RrRrrR gGGGgg BBBbbB sSSSSs
+99941:rRrrrr GggggG bbbbbb SssSss
+94972:RRRRrr GGgGGg bBbBBb sSSSSS
+83177:RRRrrR gGGGgG bBBBBB SSSSsS
+26310:rrrrrR GGgggg BBBBbB sSssSs
+72517:rrrRrr ggGggG BbbbbB SSsSSS
+44007:RrRrRr ggggGg bBbBbb ssSSSs
+58408:RrrRrR gGGGGG bbBBbb SSsSSs
+27549:RRrrrR GggGGg bBbBbb SssssS
+85967:rRrrrR ggGgGg BbbbbB SSsssS
+81693:RrRrRr GGGGGg bbbbBb sSSsss
+30964:rrrrrr GGgGgG BbBBbb SSsSSs
+79575:RrRRrr GgggGG BBbBbB SsSSSs
+27927:RRRRRR GgGgGG BbbBBb ssssSs
+83211:rRRrrR gGgggG BbBBBB SSSsSs
+50196:rrrRRr ggGGgG bBbbBb SssssS
+73652:RrRRrR gGGGGg bBbBBb ssssSs
+19177:rRRrrr gGGgGG bbbbbB SSSSsS
+14243:RRRrrr Gggggg BBbBBb SSsssS
+18316:rrRrRR GgGGGg BBbBbB sssSSS
+55525:RRRRrr gGGGgg BbbbBb SSsSSS
+70072:rrrRRR ggGggg BBbBBb ssSSSs
+26622:RrRRRr GGgGGG BbBBBb SsSSss
+86874:rRRRRr GgggGG bBBBBb SSSSSs
+60292:RRrRRr gGGgGG BbBBBB SSsSSS
+33292:RRrRRr gGGgGg BBBBbB sssssS
+82888:rrrrrR GGgggG Bbbbbb SssSss
+73546:rRrRRr GgGgGG BbBbBb sSsSSS
+93442:RrRRrR gggGgg BBbbBb sssSsS
+12861:RrRrrR Gggggg bbBbbb sSssSs
+83687:RRRRRR gGgggG bbBbbB SSsssS
+33999:rRrrRr gggGGg BbBBbb sssSsS
+19431:RRrRrR gggGGG bbbBBB SSSSSs
+41670:rrRRRR ggggGg bBbbbB SSSSSS
+25291:RRRRrR ggGGgg bbbBbB ssSsss
+96636:RRRrRr GGgGGg BbBbbb SsSSSS
+78129:RRRrrR GgGgGG bbBbBb SSSSSs
+72106:RRRrRr GgGGGg BbbBbb ssssss
+19179:rrRRrR GGGGGg BbbbBB sSssSs
+37685:rrRRrr GggGGG BBBbbB sSsssS
+74626:RRRRRR ggGGgG BBBBbb SSsSsS
+76073:RrrrRr GgGGgG BbBBBb Ssssss
+25766:rRrrRR GGggGG bBbbbB SssssS
+86393:rrrRRR GgGggg BbbBbB SsSsss
+79458:RRrrRR GGGGGg bbbbbB SsSsss
+16606:rrrRrR gGGggg BbBBbB SSSsss
+60908:rrrRrR GgGGGG BBBBBB sSssSs
+71115:RRrrrR ggGggg bBbBBB SSssSS
+15817:rRrRrR GGggGG bBBbBB SsSSsS
+28399:RRRRrR gggggG BBbBbb sSSSsS
+45422:RrrRrR gggGgg bbbBbb SSssSS
+84077:RRRRRr GGGggG bbbBBB ssSsSS
+15777:RRRRRr gGgGGg bBbBBB SsSSSS
+37383:rrRrrR GGGggG BbBBBB SSSssS
+28813:RRRRRr GGGgGG bBbbBB sSSsSs
+27008:RRrRRR gGgGGg BBbBBB SssSSs
+22928:rrRRRR ggGGgG bbBbBB ssSsSS
+80983:rRRrRR GGgGgg BbbBBb ssSsSS
+24099:rRRRRR GgGGGG bbbBbb sSsSSs
+27595:Rrrrrr gGGGGg bBbbbB sSSSss
+75772:rrrrrR GGgGgg BbbBBb ssSsSS
+86574:RRrrRr ggGggg BBBbBb SSSssS
+22963:RRrRrr GGGGgG BBBBBB SsSsss
+75321:rrRrrr GgggGg bbbBbB ssSsSs
+15446:rrrRRR gGGGGG bbBbBb SsSsSS
+59460:RRrrRr gggGGG BBbBBb SSsSSS
+54806:RRrRRR gggGGG BbbBbB SSSSSS
+31726:rRRRrr GgGGgG BBbBbb sSsSSS
+25821:RRRrrr gGGGgg bbBBbb ssssss
+27986:rrRRRR GgGggg bbBBbB sssSSs
+14062:rRrRrr gGGggG BBbBbb SssSss
+83082:rRrRrr ggGGGg bbBbBb sSSsSs
+78661:rrrRrr ggggGg bBbBBB SSSssS
+59840:rRrRRR GGGGgg bBBbbB sssSSs
+41556:RRRrRR GgGGgg BbBBBB sSsSSS
+83142:rRrrrR gggGGg BbbBbb ssSsSs
+79664:rRRRRr GggGgG BBBbBB sSSSSS
+91663:rRrrrr gGgGGG BbbBbB sSSSsS
+31729:rRRrRr GGGGGG BbBbBB SSsSsS
+11753:RRRrrR GGggGG bBbbBB SSSSsS
+22542:rrRrrr GgggGg bbBbbb SSsSsS
+91115:RrrrRr gggggG BbbBBb sSsSsS
+57065:RrRrRr ggGgGg BBbBbb SsSSSS
+39718:rRrrRR gGgGGg bBbBBB SSsSss
+94431:RRRrrR gGGggG BbbBBB SsSSSs
+72712:rrrrRR GGggGg bBBbBB sSSSSs
+60152:rrRRRR ggggGG bbBbbB sSssSs
+62626:rRrRrR ggGggg BbbbBB Ssssss
+78140:RrRrRR gGGGGg bbbbBb sSSsSS
+75062:rrrRRR GGgggG bbbbBb SssSSS
+58407:RrrrRR GGGGGg bbbbBb SSSSss
+17513:RRRRrr GgGggg bBBbbB sSsSSs
+14140:RRRRrr GGGggg bbbbBB Ssssss
+33768:rrRRrR GGgggg bBbbbb SSSsSS
+95256:RRRRrR ggGGGg bbBBbb sSssss
+63577:RRRRrR gGGggG BBBBBb ssSSSs
+65140:RrRRrr GGGggG bbbbbB ssSSSs
+47634:rrRrrR ggGGGG bbBbbB SSsSSS
+44781:RRRRRr GgggGG bbbbBB Ssssss
+49787:RRRRrR GgGgGG bbbBBb SsSSss
+97190:RRRrRR GgGgGg BbbBbb sSssSS
+77195:rRrRrR ggGgGg bbBBBB sSssSs
+93559:RRRRRR ggGGGg Bbbbbb sSssSs
+43846:rrrrRR gGGGGG BBBBbB SSsSSS
+57634:rRrrrr GggGgg BbBbbB sSssSS
+47818:rRrRRr gGGgGG BBbbBb sssSsS
+48054:RRRRrr ggGgGG bbbbbb SsSsss
+71699:RrRRRR GgggGg BBbBbB SssSsS
+21642:RrRRrR ggGgGg bbBBbB SSSSsS
+38938:rrRrRR GGGGgg bbBBBb sSsSSs
+63190:rrRrrR GGgGgG BbBBBb sssSSs
+93784:rRrrRR ggGGGg bBbbbb SSSSsS
+61195:rRrRrr GgGGGg bBBBbb ssSSSs
+11473:RRrrrr ggGGgg BbBbbb ssSssS
+61409:rRrrRr GGGgGG bBBbBB SSssss
+39344:rrrRRR ggGgGg bbBBBb SsSSSS
+78669:RrRrRR GGggGG bBBBBb SSSssS
+99723:rRRrrR GGgGGg bbBBBB SsSssS
+44293:RRRRRR GGGggG BBbbBb SsSssS
+96061:rRRRRR GGGggG bBbbBB ssssSS
+46934:rRrRRr gGgGgG bBbBbb sSSSSs
+58466:rRRrRR GGgggG BBbbBb SsssSS
+48240:RRRRRR GgGggg BbbbBB sSsSSs
+25903:rrRrrr GgGgGg bBBbBb sSSssS
+66309:rRrRRR gGgGGg BBbbbB sSSSsS
+29071:RrrrRR ggggGG BBBBbb ssSssS
+85471:RrrRrr ggggGG BBbbBB SsSsSS
+36639:RRrrrR GgGggg bBBBBB sSsSsS
+55354:RrRrRr gGgGgg bbbBBB ssSsss
+87607:rRrRrR ggGgGG bBBBbb sSSSss
+72674:RrrRrr gGGgGG bbbBbB SSsssS
+51397:rRrrRR GGgggg BbbBBB ssssss
+62993:rrRrrr gGGggG BBbBbb sssSSs
+18328:RrRRRR GGggGG bbbBbb sSsSSS
+93896:rrrrrR GGGggg BBBBbB SssSSS
+22539:RrRrRR GgGGgG bBBbbb ssSSSS
+36985:RrrrRr GggGgg BBbbBB ssSsSS
+84966:RrrRRR GGgGgg BbBbbB sSssSs
+70263:rrRrRr gGGggg bBBbBB SSSSsS
+39823:rRrrrr gggGGG bbbbbb sSsSsS
+76018:RRRRRr gggGGG bbBbBb ssSSsS
+53098:RrrRRR gggGGg BbbBBb sSSsss
+34803:RRrRRR GGGgGG BbBbBB sSsSSS
+69038:RRRRRr ggGGGG BbBBBb SSSSSs
+35116:rRRRRr GgGggG bbBBbB SSssSS
+21981:RrRrRR GGGggg bBBBBb SSsSSS
+56280:rrrrRr GGgggG bBBbbb SSSSSS
+65108:rRrRrR gGggGG bbbBbB SSssSs
+35858:RRRRrr GGGGGg BBBBbB sSssSS
+46187:rrrRrr ggGGGG BbbBbB SSSSSS
+85999:RrrRrR gGGggG bBbbBB ssSSSS
+78875:rRRrRR ggGGGG bbBBbb SSssss
+86410:rrRrRR GgGGgg bbbbBb SSSsSs
+81514:rRRRrR GgGGGg bbbbBb SSSSSs
+50244:rRrrRr gGGGgg bbBBbb sSssSS
+28682:RrrRrR gGgggg BBbBbB sssSsS
+18206:RrrrRr GGGgGG BbBBbb sSsssS
+87131:rRRRrr GgGggg BbbBBb sssSSs
+56032:RRrrRR GGggGg BbbbBb SsSSSS
+73182:rRRRRR ggGGGg BBbBBB sSSsss
+33937:RrrRRR GGGgGg BbbBBb sssSSS
+29190:rRrrrr ggGGgG BbbBbb SSSsss
+66659:rRRRRr gggggG bBBBbb SSsSss
+63557:rRrrRR gggGGg BBBBBb sSSSss
+49867:RrrrRR gGgGgG BbBBbb sSsssS
+26706:RRRRrr GgGGGg bBBBBb SSSSSS
+71815:rrrrrr gggGgG bBBBbB SSssSs
+31189:RRRrrR GGGGGg BBbbbB ssSSSS
+51095:rrrRRr ggGgGG BbbbbB SSsSss
+79065:rRrrrR gGGgGg bBBbBb sSssSs
+84080:RrRrrR ggGgGG BBbbbB sssSss
+73827:rRRRrR GgGGGG BbBBbb sSsSss
+12234:rRRRRr GggGgG BBBbbb SSssSs
+93810:RRRrrr gggGGG BBBbbb SsSsss
+85066:RrrrrR gGGgGG bbBbbB Ssssss
+23553:RRrrRR gGGgGg BBbBBb SSSSss
+36965:RRRrrR GGgGgG BbbbBb sssSSS
+81289:rRrrrr GgGgGg bbbbBB SSsSSs
+15565:RrRrrr GgGgGg bbBBbb ssSsss
+69384:RrrrrR GgggGg BbbbBB SSSSss
+46979:RrrRrR ggggGg bbbBBb sSSSSS
+21491:RRRrrr GGgGgG bBBBbB SssssS
+31454:rrRRrR gGGGGg bbbbBB SssSsS
+74411:RrRrrr GggggG bbbbbb SSSsSs
+77742:rRrRRr gGGgGG bBbbBB SSSSsS
+28687:RrRRrr GGgGGg BbBbbB SsSsSs
+50622:RrrRrr GGGggg bBbbBB ssSsss
+53093:rrRrrR GgGgGg BBbBbb SSsSss
+90352:RRrrRR gggGGg bbbBbb SsSssS
+71791:rRrRrR gggGgg bbbBBb sssSSs
+72280:RRRrRR gggGGG bbbbbB ssSSSs
+83157:RrRRrr GGGgGg bBbbBB SsSSSs
+17449:RrrRrR GgGgGg BBbBbb Ssssss
+30776:RRrrRr gGggGg BBBBBB Ssssss
+17277:rrRRrR GggGgg bbbbBB ssssSS
+65065:RrrRrr gGgGGG bBbBbb sSSsSS
+21102:RRRRrR GgGGGg BbbBBB sSSSSs
+72177:RRRrrr gGGGgg BbBbBb SssSSs
+72784:RrRRrR gGgggg bbbBBB sSSSsS
+34683:RRRRRR gGGgGG BBBbbB SssSSs
+57011:RrrrRR gGggGg BBBbBB Ssssss
+32016:RrRrrr GgGgGG bbbBbb ssSSss
+69060:RrrRRr gGgGGg BbbBBb sSsssS
+34782:rrRRRR Gggggg bBBbbb SsSSss
+88143:rRRRRR gGGgGG BBbbbB sSsSsS
+34886:RRrrRR gGggGg BbBbbb SsSsSs
+87861:RRRrrR gGGGGg BbBBbb sSSsSS
+40024:RRRRrR gggggG BBBbBb SsssSs
+88976:RrRRrr GgGGGg BBbbBb SSSSSs
+96761:RrrRRR ggGggg BbbbBB SssSSs
+16152:rrRRRR gggggg BBBbBb ssSssS
+44936:RrrRRR GGGGGg BBbBBb SSSsSS
+51627:rrrrrR gGgGgG BbBbbB SsssSS
+31408:RrRrrR Gggggg bbBBBB SSsSss
+44349:RrRrRr gggGGG BBbbbb SSssSs
+72649:RrrRrr ggggGg bBBBBB SsssSs
+97557:RrrRRr GggGGG bBbbbB SsSssS
+23612:rRRRrr gggGgG Bbbbbb ssSSss
+87995:RrRRRr GgGGGg BBbBbb ssSsSs
+54396:Rrrrrr GgggGg bBbBbb SSssSS'''
+
 import re
 from dataclasses import dataclass, field
 
-pattern = r'^(\d{4}):([rR]{6}) ([gG]{6}) ([bB]{6})$'
+pattern = r'^(\d{4,5}):([rR]{6}) ([gG]{6}) ([bB]{6})(?: ([sS]{6}))?$'
 
 @dataclass
 class Scale:
-    identfier: str
+    identifier: str
     red: str
     green: str
     blue: str
@@ -16,14 +273,27 @@ class Scale:
     dominant_color: str = field(init=False)
 
     def __post_init__(self):
+        self.identifier = int(self.identifier)
         self.red_value = int(self.red.replace('r', '0').replace('R', '1'), 2)
         self.green_value = int(self.green.replace('g', '0').replace('G', '1'), 2)
         self.blue_value = int(self.blue.replace('b', '0').replace('B', '1'), 2)
+        if self.shine:
+            self.shine_value = int(self.shine.replace('s', '0').replace('S', '1'), 2)
         self.dominant_color = max((self.red_value, 'red'), 
                                   (self.green_value, 'green'), 
                                   (self.blue_value, 'blue'))[1]
 
+scales = []
 
-line = '2456:rrrrrr ggGgGG bbbbBB'
+for line in lines.split('\n'):
+    match = re.match(pattern, line)
+    scales.append(Scale(*match.groups()))
 
-print(Scale('2456', 'rrRrrr', 'GGGGGG', 'BbBbBb'))
+print(sum(scale.identifier for scale in scales if scale.dominant_color == 'green'))
+
+sorted_scales = sorted(scales, key=lambda scale: (-scale.shine_value, sum((scale.red_value, scale.green_value, scale.blue_value))))
+max_shine = sorted_scales[0].shine_value
+
+# Veronica asks you to find the scales of individuals with the highest level of shine, 
+# and among them choose the darkest one (the one with the lowest total sum of colour components). 
+print(sorted_scales[0].identifier)
